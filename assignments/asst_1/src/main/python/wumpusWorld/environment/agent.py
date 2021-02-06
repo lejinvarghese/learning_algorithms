ba2c00a2182264b7b1c89502d62d2aa50c9252e3
Lejin Varghese <lejinsnests@yahoo.com>
2021-02-06 14:46:16 -0500

    sync

--- assignments/asst_1/src/main/python/wumpusWorld/environment/agent.py
+++ assignments/asst_1/src/main/python/wumpusWorld/environment/agent.py
@@ -1,8 +1,8 @@
 # import scala.math.{max, min}
-from environment import Coords, Direction
+from environment import Coordinates, Direction, Orientation
 
-class Agent:
-  def __init__(self, location, orientation, has_gold, has_arrow, is_alive):
+class Agent(object):
+  def __init__(self, location = Coordinates(0, 0), orientation = Orientation(), has_gold=False, has_arrow=True, is_alive=True):
     self.location = location
     self.orientation = orientation
     self.has_gold = has_gold
@@ -15,24 +15,27 @@ class Agent:
 
   def turn_left (self):
     newAgent = Agent.__copy__(self)
-    newAgent.orientation.turn_left()
+    newAgent.orientation.turn_left(self)
     return newAgent
   
   def turn_right (self):
     newAgent = Agent.__copy__(self)
-    newAgent.orientation.turn_right()
+    newAgent.orientation.turn_right(self)
     return newAgent
 
   def forward(self, grid_width, grid_height):
     new_location = self.location
-    if self.orientation == Direction.west:
-      new_location = Coords(max(0, self.location.x - 1), self.location.y)
-    elif self.orientation == Direction.east:
-      new_location = Coords(min(grid_width -1, self.location.x + 1), self.location.y)
-    elif self.orientation == Direction.south:
-      new_location = Coords(max(0, self.location.y - 1), self.location.x)
-    elif self.orientation == Direction.north:
-      new_location = Coords(min(grid_height -1, self.location.y + 1), self.location.x)
+    orientation_value = self.orientation.orientation
+    if orientation_value == Direction.west:
+      new_location = Coordinates(max(0, self.location.x - 1), self.location.y)
+    elif orientation_value == Direction.east:
+      new_location = Coordinates(min(grid_width -1, self.location.x + 1), self.location.y)
+      # print(new_location.x, new_location.y)
+    elif orientation_value == Direction.south:
+      new_location = Coordinates(self.location.x, max(0, self.location.y - 1))
+    elif orientation_value == Direction.north:
+      new_location = Coordinates(self.location.x, min(grid_height -1, self.location.y + 1))
     newAgent = Agent.__copy__(self)
     newAgent.location = new_location
+    return newAgent
      
--- assignments/asst_1/src/main/python/wumpusWorld/environment/test_environment.py
+++ assignments/asst_1/src/main/python/wumpusWorld/environment/test_environment.py
@@ -1,16 +1,35 @@
 from environment import Action, Percept, Orientation
 from numpy.random import randint
 from agent import Agent
+from environment import Coordinates, Direction
 
 action = Action(randint(1,6))
 print(action)
 
+print(Direction(2), Direction(1))
+
 percept = Percept().show()
 print(percept)
 
-orient = Orientation()
-print(orient.active_orientation)
-orient.turn_left()
-print(orient.active_orientation)
+# orient = Orientation()
+# print(orient.orientation)
+# orient.turn_left()
+# print(orient.orientation)
 
 
+# location = Coordinates(0, 0)
+
+agent = Agent()
+print(agent.orientation.orientation)
+print(agent.location.x, agent.location.y)
+
+agent.orientation.turn_left()
+print(agent.orientation.orientation)
+print(agent.location.x, agent.location.y)
+
+for i in range(6):
+    agent = agent.forward(4, 4)
+    if i==3:
+        agent.orientation.turn_right()
+    print(agent.orientation.orientation)
+    print(agent.location.x, agent.location.y)
