175e406ecc7589f5c5b9f6cae7cd48a706e67b88
Lejin Varghese <lejinsnests@yahoo.com>
2021-03-03 22:41:34 -0500

    v

--- assignments/assignment_3/src/main/python/wumpus_world/test.py
+++ assignments/assignment_3/src/main/python/wumpus_world/test.py
@@ -9,31 +9,76 @@ from environment.environment import (
 import numpy as np
 from agent.agent import Agent, BeelineAgent
 from beeline_world import main
-from pomegranate import DiscreteDistribution, ConditionalProbabilityTable, Node
-
-p01 = DiscreteDistribution({'y': 0.2, 'n': 0.8})
-p10 = DiscreteDistribution({'y': 0.2, 'n': 0.8})
-p11   = DiscreteDistribution({'y': 0.2, 'n': 0.8})
-
-OC = ConditionalProbabilityTable([[0, 0, 0, 0.999],
-                                  [0, 0, 1, 0.001],
-                                  [0, 1, 0, 0.750],
-                                  [0, 1, 1, 0.250],
-                                  [1, 0, 0, 0.700],
-                                  [1, 0, 1, 0.300],
-                                  [1, 1, 0, 0.050],
-                                  [1, 1, 1, 0.950]], [BRCA1, BRCA2])
+import matplotlib.pyplot as plt
+from pomegranate import BayesianNetwork, DiscreteDistribution, ConditionalProbabilityTable, Node
+from pomegranate.utils import plot_networkx
+from itertools import product
 
 
+def generate_cpt_n(n_sides):
+    n_sides = n_sides+1
+    _matrix = np.zeros(n_sides)
+
+    for i in range(1, 2**n_sides):
+        _binary = np.array([int(x)
+                            for x in list(np.binary_repr(i, width=n_sides))])
+        _matrix = np.vstack((_matrix, _binary))
+
+    _any_pit = np.max(_matrix[:, :n_sides-1], axis=1)
+    _any_breeze = _matrix[:, -1]
+
+    _proba = np.expand_dims(np.invert(np.logical_xor(
+        _any_pit, _any_breeze)).astype(float), axis=1)
+    _matrix = np.hstack((_matrix, _proba))
+    return _matrix
 
 
+p01 = DiscreteDistribution({1: 0.2, 0: 0.8})
+p10 = DiscreteDistribution({1: 0.2, 0: 0.8})
+p11 = DiscreteDistribution({1: 0.2, 0: 0.8})
+
+grid_width, grid_height = 3, 3
+pits = {}
+nodes = {}
+breezes = {}
+
+for x, y in product(range(grid_width), range(grid_height)):
+    pits[str(x)+'_'+str(y)] = DiscreteDistribution({1: 0.2, 0: 0.8})
+    nodes[str(x)+'_'+str(y)] = Node(pits[str(x) +
+                                         '_'+str(y)], name=str(x)+'_'+str(y))
+
+for x, y in product(range(grid_width), range(grid_height)):
+    try:
+        breezes[str(x)+'_'+str(y)] = ConditionalProbabilityTable(generate_cpt_n(2),
+                                                                 [pits[str(x+1)+'_'+str(y)], pits[str(x)+'_'+str(y+1)]])
+    except:
+        pass
+
+print(breezes, len(breezes))
 
 
+# print(generate_cpt_n(n_sides=4))
 
+b00 = ConditionalProbabilityTable([[0, 0, 0, 1.0],
+                                   [0, 0, 1, 0.0],
+                                   [0, 1, 0, 0.0],
+                                   [0, 1, 1, 1.0],
+                                   [1, 0, 0, 0.0],
+                                   [1, 0, 1, 1.0],
+                                   [1, 1, 0, 0.0],
+                                   [1, 1, 1, 1.0]], [p01, p10])
 
+s1 = Node(p01, name="p01")
+s2 = Node(p10, name="p10")
+s3 = Node(b00, name="b00")
 
+model = BayesianNetwork("Monty Hall Problem")
+model.add_states(s1, s2, s3)
+model.add_edge(s1, s3)
+model.add_edge(s2, s3)
+model.bake()
 
-
+# print(model.predict_proba([{'b00': 1}]))
 
 # n_simulations = 300
 # n_success = 0
