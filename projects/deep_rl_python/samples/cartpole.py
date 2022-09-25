d0744b35168fdb4c90ec85feb5ea1c10ea970aae
lejinvarghese <lejinsnests@gmail.com>
2022-09-25 13:43:44 -0400

    basic changes
    Signed-off-by: lejinvarghese <lejinsnests@gmail.com>

--- projects/deep_rl_python/samples/cartpole.py
+++ projects/deep_rl_python/samples/cartpole.py
@@ -1,17 +1,17 @@
 import gym
 
 n_episodes = 20
-n_log_frequency = n_episodes // 5
+n_log_frequency = n_episodes // 10
 n_timesteps = 50
 
 if __name__ == "__main__":
     env = gym.make("CartPole-v1", render_mode="human")
 
-    for e in range(n_episodes):
+    for e in range(1, n_episodes + 1):
         episode_return = 0
         state = env.reset()
 
-        for t in range(n_timesteps):
+        for t in range(1, n_timesteps + 1):
             env.render()
             random_action = env.action_space.sample()
             next_state, reward, terminated, truncated, info = env.step(
@@ -19,10 +19,10 @@ if __name__ == "__main__":
             )
             episode_return += reward
             if terminated:
-                print(f"Episode finished after {t+1} timesteps")
-                print(f"Episode return: {episode_return}")
                 break
         if e % n_log_frequency == 0:
-            print(f"Episode: {e}, Return: {episode_return}")
+            print(
+                f"Episode {e}, finished after {t} timesteps, with a return {episode_return}."
+            )
 
     env.close()
