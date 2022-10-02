02e98ebc740eb2ba27a86bc9783ac4811c2c3256
lejinvarghese <lejinsnests@gmail.com>
2022-10-02 17:58:31 -0400

    policy iteration added
    Signed-off-by: lejinvarghese <lejinsnests@gmail.com>

--- projects/deep_rl_python/samples/dynamic_programming/policy_iteration.py
+++ projects/deep_rl_python/samples/dynamic_programming/policy_iteration.py
@@ -9,13 +9,15 @@ except ImportError:
         extract_policy,
     )
 
+
 n_episodes = 10
 n_log_frequency = max(1, n_episodes // 10)
 n_timesteps = 50
 
 
-def value_iteration(
+def compute_value_function(
     env: gym.Env,
+    policy: List[int],
     n_iterations: int = 1000,
     convergence_threshold: float = 1e-20,
     gamma: float = 1.0,
@@ -33,40 +35,69 @@ def value_iteration(
         value_states: a list of values for each state
     """
     _n_states = env.observation_space.n
-    _n_actions = env.action_space.n
     _value_states = np.zeros(_n_states)
 
     for i in range(n_iterations):
         _next_value_states = np.copy(_value_states)
         for s in range(_n_states):
-            _q_states_actions = [
-                sum(
-                    [
-                        trans_proba
-                        * (reward + gamma * _next_value_states[next_state])
-                        for trans_proba, next_state, reward, _ in env.P[s][a]
-                    ]
-                )
-                for a in range(_n_actions)
-            ]
-            _value_states[s] = max(_q_states_actions)
+            a = policy[s]
+            _value_states[s] = sum(
+                [
+                    trans_proba
+                    * (reward + gamma * _next_value_states[next_state])
+                    for trans_proba, next_state, reward, _ in env.P[s][a]
+                ]
+            )
 
         if (
             np.sum(np.fabs(_next_value_states - _value_states))
             <= convergence_threshold
         ):
             break
-    print(f"Value-iteration converged at iteration {i+1}.")
+
     return np.round(_value_states, 4)
+
+
+def policy_iteration(
+    env: gym.Env,
+    n_iterations: int = 1000,
+) -> List[float]:
+    """
+    Goal: Compute the optimal policy.
+
+    Args:
+        env : gym environment
+        n_iterations : number of iterations
+        convergence_threshold : threshold for convergence
+        gamma : discount factor
+
+    Returns:
+        value_states: a list of values for each state
+    """
+
+    _policy = np.zeros(env.observation_space.n)
+
+    for i in range(n_iterations):
+
+        _value_states = compute_value_function(env, _policy)
+
+        _next_policy = extract_policy(env, _value_states)
+        if np.all(_policy == _next_policy):
+            break
+
+        _policy = _next_policy
+
+    print(f"Policy-iteration converged at iteration {i+1}.")
+    return _policy
 
 
 if __name__ == "__main__":
     env = gym.make("FrozenLake-v1", render_mode="human")
     state = env.reset()
     total_returns = 0
-    optimal_value_states = value_iteration(env)
-    optimal_policy = extract_policy(env, optimal_value_states)
-    print(f"Optimal policy obtained through value iteration: {optimal_policy}")
+
+    optimal_policy = policy_iteration(env)
+    print(f"Optimal policy obtained through policy iteration: {optimal_policy}")
 
     for e in range(n_episodes):
         episode_return = 0
