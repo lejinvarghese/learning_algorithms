e76f38738d1742139ce48bad95ea3130240d6a53
lejinvarghese <lejinsnests@gmail.com>
2024-03-16 04:52:24 -0400

    change existing folders
    Signed-off-by: lejinvarghese <lejinsnests@gmail.com>

--- deep_learning/explain/setup.sh
+++ deep_learning/explain/setup.sh
@@ -1,4 +1,4 @@
 python -m venv .venv
 source .venv/bin/activate
 pip3 install -r requirements.txt
-python3 -m ipykernel install --user --name interpret --display-name explain
+python3 -m ipykernel install --user --name explain --display-name explain
