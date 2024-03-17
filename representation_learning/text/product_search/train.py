016e9f3bf8f034697dd0506df8966062fe68a5a9
lejin <lejinsnests@gmail.com>
2024-03-16 22:04:39 -0400

    change args
    Signed-off-by: lejin <lejinsnests@gmail.com>

--- representation_learning/text/product_search/train.py
+++ representation_learning/text/product_search/train.py
@@ -1,5 +1,7 @@
 from typing import Tuple
 import logging
+import argparse
+
 import pandas as pd
 
 logging.basicConfig(level=logging.INFO)
@@ -9,18 +11,19 @@ SRC_PREFIX = "shopping_queries_dataset"
 
 
 def preprocess(cache: bool = True) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    filters = [("small_version", "==", 1)]
+    filters = [("small_version", "==", 1), ("product_locale", "==", "us")]
     judgments = pd.read_parquet(
         f"{DIRECTORY}/{SRC_PREFIX}_examples.parquet", filters=filters
     )
-    products = pd.read_parquet(f"{DIRECTORY}/{SRC_PREFIX}_products.parquet")
+    products = pd.read_parquet(
+        f"{DIRECTORY}/{SRC_PREFIX}_products.parquet", filters=filters[1:]
+    )
 
     data = pd.merge(
         judgments,
         products,
         how="left",
-        left_on=["product_locale", "product_id"],
-        right_on=["product_locale", "product_id"],
+        on="product_id",
     )
 
     train = data[data["split"] == "train"]
@@ -34,5 +37,13 @@ def preprocess(cache: bool = True) -> Tuple[pd.DataFrame, pd.DataFrame]:
     return train, test
 
 
+def main(args):
+    preprocess(args)
+
+
 if __name__ == "__main__":
     preprocess()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--random_state", type=int, default=42, help="Random seed.")
+    parser.add_argument("--train_batch_size", type=int, default=32, help="Batch size.")
+    args = parser.parse_args()
