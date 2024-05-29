236cda64a9033c2c9cf3e7f5e28e1630a48a5cb8
lejinvarghese <lejin.varghese@shopify.com>
2024-05-29 15:09:41 -0400

    test eval
    Signed-off-by: lejinvarghese <lejin.varghese@shopify.com>

---
+++ representation_learning/text/product_search/retrieval/train.py
@@ -0,0 +1,91 @@
+import numpy as np
+from datasets import load_dataset
+from sentence_transformers import SentenceTransformer, SentenceTransformerTrainer
+from sentence_transformers.losses import MultipleNegativesRankingLoss
+from sentence_transformers.training_args import SentenceTransformerTrainingArguments
+from sentence_transformers.training_args import BatchSamplers
+from sentence_transformers.evaluation import TripletEvaluator
+from multiprocessing import cpu_count
+from transformers.integrations import TensorBoardCallback
+
+esci = load_dataset(
+    "smangrul/amazon_esci", num_proc=cpu_count() - 2, ignore_verifications=True
+)
+esci_train_df = esci["train"].to_pandas()
+esci_valid_df = esci["validation"].to_pandas()
+
+
+def build_multi_neg_dataset(df):
+    df["label"] = np.where(df["esci_label"].isin(["E"]), 1, 0)
+    pos = df[df["label"] == 1]
+    pos_nec = pos[["query", "product_title"]]
+    pos_nec.columns = ["query", "pos"]
+
+    neg = df[df["label"] == 0]
+    neg_nec = neg[["query", "product_title"]]
+    neg_nec.columns = ["query", "neg"]
+
+    num_samples = len(pos)
+    posneg = pos_nec.merge(neg_nec, on="query", how="inner").sample(
+        int(num_samples / 20)
+    )
+
+    return Dataset.from_pandas(posneg, preserve_index=False)
+
+
+training_dataset, validation_dataset = build_multi_neg_dataset(
+    esci_train_df
+), build_multi_neg_dataset(esci_valid_df)
+
+
+# 1. Load a model to finetune
+model = SentenceTransformer("nomic-ai/nomic-embed-text-v1.5", trust_remote_code=True)
+
+# 3. Define a loss function
+loss = MultipleNegativesRankingLoss(model)
+
+args = SentenceTransformerTrainingArguments(
+    # Required parameter:
+    output_dir="models/nomic-embed-text-esci",
+    # Optional training parameters:
+    num_train_epochs=1,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
+    warmup_ratio=0.1,
+    fp16=True,  # Set to False if GPU can't handle FP16
+    bf16=False,  # Set to True if GPU supports BF16
+    batch_sampler=BatchSamplers.NO_DUPLICATES,  # MultipleNegativesRankingLoss benefits from no duplicates
+    # Optional tracking/debugging parameters:
+    eval_steps=100,
+    save_strategy="steps",
+    save_steps=100,
+    save_total_limit=2,
+    logging_steps=1,
+    run_name="nomic-embed-text-esci",  # Used in W&B if `wandb` is installed
+)
+
+# dev_evaluator = TripletEvaluator(
+#     anchors=eval_dataset["anchor"],
+#     positives=eval_dataset["positive"],
+#     negatives=eval_dataset["negative"],
+#     name="all-nli-dev",
+# )
+# dev_evaluator(model)
+# 4. Create a trainer & train
+trainer = SentenceTransformerTrainer(
+    model=model,
+    args=args,
+    train_dataset=training_dataset,
+    eval_dataset=validation_dataset,
+    loss=loss,
+    callbacks=[TensorBoardCallback()],
+)
+trainer.train()
+
+# test_evaluator = TripletEvaluator(
+#     anchors=test_dataset["anchor"],
+#     positives=test_dataset["positive"],
+#     negatives=test_dataset["negative"],
+#     name="all-nli-test",
+# )
+# test_evaluator(model)
